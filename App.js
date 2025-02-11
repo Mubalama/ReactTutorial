b8e96577710e8e9bf3581b68a277fdb0ebe821d8
Mubalama Richard <mubalama@live.com>
2025-02-12 00:10:28 +1100

    Update Navigation
    Updating Navigation structure

--- App.js
+++ App.js
@@ -1,4 +1,29 @@
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{ headerTitle: 'App' }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Components" component={ComponentsScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// OLD NAVIGATION STRUCTURE
+/*import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
@@ -18,4 +43,4 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator); */
